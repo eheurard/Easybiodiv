387684146533fde5c9e7b8ae16b52edfd99fdddd
Elouan Heurard <elouan.heurard@orange.fr>
2026-05-28 14:33:09 +0200

    feat: add dashboard index view and tests

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class DashboardIndexViewTests(TestCase):
+
+    def test_index_returns_200(self):
+        response = self.client.get(reverse('dashboard:index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_uses_correct_template(self):
+        response = self.client.get(reverse('dashboard:index'))
+        self.assertTemplateUsed(response, 'dashboard/index.html')
